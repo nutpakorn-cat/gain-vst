bcb5828b04dcfef1fb3655b827c767a85054e969
nutpakorn-cat <twiceworldSoftware@gmail.com>
2020-08-27 09:59:07 +0700

    initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,45 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+HelloworldGainAudioProcessorEditor::HelloworldGainAudioProcessorEditor (HelloworldGainAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    gainSlider.setSliderStyle(Slider::Rotary);
+    gainSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 50, 20);
+    gainSlider.setRange(0.0f, 1.0f, 0.01f);
+    gainSlider.setValue(0.5f);
+    gainSlider.addListener(this);
+
+    addAndMakeVisible(gainSlider);
+    setSize (400, 300);
+}
+
+HelloworldGainAudioProcessorEditor::~HelloworldGainAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void HelloworldGainAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    g.fillAll(Colours::black);
+}
+
+void HelloworldGainAudioProcessorEditor::resized()
+{
+    gainSlider.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 75, 100, 150);
+}
+
+void HelloworldGainAudioProcessorEditor::sliderValueChanged(Slider *slider) {
+    if (slider == &gainSlider) {
+        audioProcessor.gainSlider = gainSlider.getValue();
+    }
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+using namespace juce;
+
+//==============================================================================
+/**
+*/
+class HelloworldGainAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                            public Slider::Listener
+{
+public:
+    HelloworldGainAudioProcessorEditor (HelloworldGainAudioProcessor&);
+    ~HelloworldGainAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    
+    void sliderValueChanged(Slider* slider) override;
+
+private:
+    Slider gainSlider;
+    HelloworldGainAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HelloworldGainAudioProcessorEditor)
+};
